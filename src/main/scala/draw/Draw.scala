261a8ba04ccba43753245b721e79000e9d0a54d8
Dmytro Starosud <d.starosud@gmail.com>
2016-12-20 13:50:10 +0200

    basic animation

--- src/main/scala/draw/Draw.scala
+++ src/main/scala/draw/Draw.scala
@@ -1,25 +1,40 @@
 package draw
 
+import java.awt._
+import java.awt.event.{WindowAdapter, WindowEvent}
 import javax.swing._
+
+import scala.util.Random
 
 object Draw {
   def createAndShowGUI(): Unit = {
-    //Create and set up the window.
-    val frame = new JFrame("HelloWorldSwing")
+    val animator = new AnimationPanel
+    animator.setPreferredSize(new Dimension(640, 480))
+
+    val frame = new JFrame("Animation")
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-
-    //Add the ubiquitous "Hello World" label.
-    val label = new JLabel("Hello World")
-    frame.getContentPane.add(label)
-
-    //Display the window.
+    frame.getContentPane.add(animator)
     frame.pack()
     frame.setVisible(true)
+
+    val timer = new Timer(100, _ => animator.repaint())
+    timer.start()
+
+    frame.addWindowListener(new WindowAdapter {
+      override def windowClosing(e: WindowEvent): Unit = {
+        timer.stop()
+      }
+    })
   }
 
   def main(args: Array[String]): Unit = {
-    SwingUtilities.invokeLater(new Runnable {
-      override def run(): Unit = createAndShowGUI()
-    })
+    SwingUtilities.invokeLater(() => createAndShowGUI())
+  }
+}
+
+class AnimationPanel extends JPanel(new BorderLayout) {
+  protected override def paintComponent(g: Graphics): Unit = {
+    super.paintComponent(g)
+    g.asInstanceOf[Graphics2D].drawLine(0, 0, Random.nextInt(getWidth), Random.nextInt(getHeight))
   }
 }
